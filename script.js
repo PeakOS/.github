92986bb361479635cfa25df867d6860e2f6ea098
Mauro De Smet <101141683+MauroDeSmet@users.noreply.github.com>
2024-03-31 22:23:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,4 @@ const hamburger = document.querySelector('.hamburger input');
         const sidebar = document.querySelector('.sidebar');
         hamburger.addEventListener('change', () => {
         sidebar.style.display = hamburger.checked ? 'flex' : 'none';
-        if(!hamburger.checked) {
-            sidebar.classList.add('show');
-            sidebar.classList.remove('hide');
-        } else {
-                document.getElementById("check").onclick = function(){
-                document.querySelector(".sidebar").style.animationName = "hide"
-            };
-            sidebar.classList.add('hide');
-            sidebar.classList.remove('show');
-        }
         });
